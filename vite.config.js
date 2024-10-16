aa1ba61a74469bfd20cbd43891fa364ea5d98464
Abiloye Blossom <amogbeyiteren@gmail.com>
2024-10-16 22:10:11 +0100

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import SitemapPlugin from 'vite-plugin-sitemap'; // Change to default import
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    SitemapPlugin({
+      hostname: 'https://0daysecurity.io',
+      
+    }),
+  ],
+});
